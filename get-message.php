ce6451c934e22f841b2c87fc96c67a553cb37408
Raju Khadka <rajukhadka374@gamil.com>
2020-03-30 15:51:17 -0500

    update

--- get-message.php
+++ get-message.php
@@ -1,37 +1,37 @@
 <?php
- $name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "rajukhadka374@gmail.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+
+    if (isset($POST['name']) && isset($_POST['email'])){
+        
+        $name = $_POST['name'];
+        $visitor_email = $_POST['email'];
+        $to = "rajukhadka374@gmail.com";
+        $email_subject = "New Form Submission";
+          
+        $message = $_POST['message'];
+
+        $email_from = $_POST['email'];
+        
+        $email_body = "User Name: $name.\n".
+                        "User Email: $visitor_email.\n".
+                            "User Message: $message.\n".
+
+        
+        $headers = "Form: $email_form \r\n";
+        $headers .="Reply-To: $visitor_email\r\n";
+        $headers .="MINE-Version: 1.0\r\n";
+        $headers .="Content-type: text/html; charset-utf-8";
 
 
-// if(isser($_POST['submit'])){
+        $send = mail($to,$email_subject,$email_body,$headers);
+        if($send){
+            echo'<br>';
+            echo'Thaknks for contacting me. I will be with you shortly.';
+        }else{
+            echo'error';
+        }
+        }
+        header("Location: index.html");
 
 
-//     $name = $_POST['name'];
-//     $subject = $_POST['subject'];
-    
-//     $visitor_email = $_POST['email'];
-//     $message = $_POST['message'];
-
-//     $email_from = $_POST['email'];
-//     $email_subject = "New Form Submission";
-//     $email_body = "User Name: $name.\n".
-//                     "User Email: $visitor_email.\n".
-//                         "User Message: $message.\n".
-
-//     $to = "rajukhadka374@gmail.com";
-//     $headers = "Form: $email_form \r\n";
-//     $headers .="Reply-To: $visitor_email\r\n";
-//     mail($to,$email_subject,$email_body,$headers);
-//     header("Location: index.html");
-
-//     mail();
-
-// }
+    }
 ?>
